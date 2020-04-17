84088461e78e24778a21df196bfb660f12eb654d
Alexander <skohliks1@gmail.com>
2020-04-17 04:18:24 -0400

    Initial commit.

---
+++ source/UnigineEcs/Core.cs
@@ -0,0 +1,20 @@
+﻿using Unigine;
+using World = Flecs.World;
+
+namespace UnigineECS
+{
+    internal static class Core
+    {
+        const uint version = (0 << 16) | (0 << 8) | (1);
+        static string stringVersion => (version >> 16 & 0xFF) + "." + (version >> 8 & 0xFF) + "." + (version & 0xFF);
+
+        public static void Init()
+        {
+            Log.Message("{0}", $"\nUnigineECS {stringVersion} has been initialized.\n");
+        }
+
+        public static void Update()
+        {
+        }
+    }
+}
